ca489424c6953b60a1c6862602078b29010ec914
webdevmahdi <mahdihasan01705259@gmail.com>
2023-10-24 10:07:20 +0600

    connect to the mongo db server

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
 
-// coffeeStore
-// ASlER5JkNGvwOfwD
-
-
-
 app.get('/', (req, res) => {
     res.send('Coffee shop server is running');
 })
 
 
-const uri = "mongodb+srv://coffeeStore:<password>@cluster0.obfo8nr.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obfo8nr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
