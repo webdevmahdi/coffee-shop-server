5c8dc2f10193848377f49721687ce276a2787835
webdevmahdi <mahdihasan01705259@gmail.com>
2023-10-24 09:46:16 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
 
+// coffeeStore
+// ASlER5JkNGvwOfwD
+
+
+
 app.get('/', (req, res) => {
     res.send('Coffee shop server is running');
 })
+
+
+const uri = "mongodb+srv://coffeeStore:<password>@cluster0.obfo8nr.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`Server is running in port : ${port}`);
